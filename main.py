5afa9f1cb36a48921ad45099ed3530a1fb0f9d50
zhulin211 <zhulin211@gmail.com>
2023-12-07 09:43:09 -0500

    test

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+class Washer():
+    def wash(self):
+        print('able to wash')
+        print(self)
+
+
+haier1 = Washer()
+haier1.wash()
+
+haier1.width  = 400
+haier1.height = 500
+
+print(f'haier1 width is {haier1.width}')
+print(f'haier1 heightis {haier1.height}')
+
+
+
+
+
+
+
+
+
+
+
+
+
